e85b6667ea8a178a12856d5cf8f9aebf28b686cd
Takahiro Noda <takahiro.noda@gmail.com>
2013-02-15 17:21:48 +0900

    Add the Scorm2004::NavigationRequestProcess module.
    Its first commit supports the following navigation requests:
    
    + Start
    + Resume All
    + Continue
    + Previous
    + Choice

---
+++ lib/scorm2004/sequencing/navigation_request_process.rb
@@ -0,0 +1,77 @@
+require 'scorm2004/sequencing'
+
+module Scorm2004
+  module Sequencing
+    module NavigationRequestProcess
+      # Navigation Request Process [NB.2.1]
+      # For a navigation request and possibly a specified activity, returns
+      # the validity of the navigation request; may return a termination
+      # request, a sequencing request, and/or a target activity; may return an
+      # exception code:
+      #
+      # @param tree [ActivityTree] The activity tree.
+      # @param req [Symbol] The navigation request.
+      # @param target [String] The target activity's identifier, which can be omitted.
+      # @return [Array] An array of [termination_request, sequencing_request].
+      def navigation_request_process(tree, req, target = nil)
+        case req
+        when :start
+          start_navigation_request_process(tree, req)
+        when :resume_all
+          resume_all_navigation_request_process(tree, req)
+        when :continue
+          continue_navigation_request_process(tree, req)
+        when :previous
+          previous_navigation_request_process(tree, req)
+        when :choice
+          choice_navigation_request_process(tree, req, target)
+        end
+      end
+
+      private
+
+      def start_navigation_request_process(tree, req)
+        sequencing_exception('NB.2.1-1') if tree.current_activity
+        [nil, :start]
+      end
+
+      def resume_all_navigation_request_process(tree, req)
+        sequencing_exception('NB.2.1-1') if tree.current_activity
+        sequencing_exception('NB.2.1-3') unless tree.suspended_activity
+        [nil, :resume_all]
+      end
+
+      def continue_navigation_request_process(tree, req)
+        c = tree.current_activity or sequencing_exception('NB.2.1-2')
+        if c.root? || !c.parent.sequencing_control_flow
+          sequencing_exception('NB.2.1-4')
+        end
+        [c.active? ? :exit : nil, :continue]
+      end
+
+      def previous_navigation_request_process(tree, req)
+        c = tree.current_activity or sequencing_exception('NB.2.1-2')
+        sequencing_exception('NB.2.1-6') if c.root?
+        p = c.parent
+        if !p.sequencing_control_flow || p.sequencing_control_forward_only
+          sequencing_exception('NB.2.1-5')
+        end
+        [c.active? ? :exit : nil, :previous]
+      end
+
+      def choice_navigation_request_process(tree, req, target)
+        t = tree.activities[target] or sequencing_exception('NB.2.1-11')
+        unless t.root? || t.parent.sequencing_control_choice
+          sequencing_exception('NB.2.1-11')
+        end
+        c = tree.current_activity
+        if c && c.active?
+          sequencing_exception('NB.2.1-8') unless c.sequencing_control_choice_exit
+          [:exit, :choice]
+        else
+          [nil, :choice]
+        end
+      end
+    end
+  end
+end
---
+++ spec/scorm2004/sequencing/navigation_request_process_spec.rb
@@ -0,0 +1,307 @@
+require 'spec_helper'
+require 'scorm2004/sequencing/navigation_request_process'
+require 'scorm2004/sequencing/sequencing_exception'
+require 'ostruct'
+
+describe Scorm2004::Sequencing::NavigationRequestProcess do
+  subject do
+    Object.new
+      .extend(Scorm2004::Sequencing::SequencingException)
+      .extend(Scorm2004::Sequencing::NavigationRequestProcess)
+  end
+  let(:tree) { OpenStruct.new }
+  let(:target) { nil }
+  let(:process) { subject.navigation_request_process(tree, req, target) }
+
+  shared_examples_for 'sequencing exception' do
+    it 'throws a sequencing exception' do
+      expect { process }.to throw_symbol(:sequencing_exception)
+      subject.exception.should be
+    end
+  end
+
+  describe 'Start navigation request' do
+    let(:req) { :start }
+
+    context 'when the current activity is defined' do
+      before { tree.current_activity = :dummy }
+      it_behaves_like 'sequencing exception'
+    end
+
+    context 'when the current activity is not defined' do
+      it 'issues a Start sequencing request' do
+        process.should == [nil, :start]
+      end
+    end
+  end
+
+  describe 'Resume All navigation request' do
+    let(:req) { :resume_all }
+
+    context 'when the current activity is defined' do
+      before { tree.current_activity = :dummy }
+      it_behaves_like 'sequencing exception'
+    end
+
+    context 'when the suspended activity is defined' do
+      before { tree.suspended_activity = :dummy }
+      it 'issues a Resume All sequencing request' do
+        process.should == [nil, :resume_all]
+      end
+    end
+
+    context 'when the suspended activity is not defined' do
+      it_behaves_like 'sequencing exception'
+    end
+  end
+
+  describe 'Continue navigation request' do
+    let(:req) { :continue }
+
+    context 'when the current activity is not defined' do
+      it_behaves_like 'sequencing exception'
+    end
+
+    context 'when the current activity is defined' do
+      context 'and is the root activity' do
+        before do
+          tree.current_activity = double('current activity')
+          tree.current_activity.stub(:root?).and_return(true)
+        end
+        it_behaves_like 'sequencing exception'
+      end
+
+      context 'and has a parent' do
+        before do
+          tree.current_activity = OpenStruct.new(:root? => false)
+          tree.current_activity.parent = double('parent activity')
+        end
+
+        context 'whose Sequencing Control Flow is False' do
+          before { tree.current_activity.parent.stub(:sequencing_control_flow).and_return(false) }
+          it_behaves_like 'sequencing exception'
+        end
+
+        context 'whose Sequencing Control Flow is True' do
+          before { tree.current_activity.parent.stub(:sequencing_control_flow).and_return(true) }
+
+          context 'when the current activity is active' do
+            before { tree.current_activity.stub(:active?).and_return(true) }
+            it 'terminates the current activity first' do
+              process.should == [:exit, :continue]
+            end
+          end
+
+          context 'when the current activity is not active' do
+            before { tree.current_activity.stub(:active?).and_return(false) }
+            it 'issues the Continue sequencing request' do
+              process.should == [nil, :continue]
+            end
+          end
+        end
+      end
+    end
+  end
+
+  describe 'Previous navigation request' do
+    let(:req) { :previous }
+
+    context 'when the current activity is not defined' do
+      it_behaves_like 'sequencing exception'
+    end
+
+    context 'when the current activity is defined' do
+      context 'and is the root activity' do
+        before do
+          tree.current_activity = double('current activity')
+          tree.current_activity.stub(:root?).and_return(true)
+        end
+        it_behaves_like 'sequencing exception'
+      end
+
+      context 'and has a parent' do
+        before do
+          tree.current_activity = OpenStruct.new(:root => false)
+          tree.current_activity.parent = double('parent activity')
+        end
+
+        context 'whose Sequencing Control Flow is false' do
+          before do
+            p = tree.current_activity.parent
+            p.stub(:sequencing_control_flow).and_return(false)
+            p.stub(:sequencing_control_forward_only).and_return(false)
+          end
+          it_behaves_like 'sequencing exception'
+        end
+
+        context 'whose Sequencing Control Forward Only is true' do
+          before do
+            p = tree.current_activity.parent
+            p.stub(:sequencing_control_flow).and_return(true)
+            p.stub(:sequencing_control_forward_only).and_return(true)
+          end
+          it_behaves_like 'sequencing exception'
+        end
+
+        context 'which allows the flow sequencing request from the current activity' do
+          before do
+            p = tree.current_activity.parent
+            p.stub(:sequencing_control_flow).and_return(true)
+            p.stub(:sequencing_control_forward_only).and_return(false)
+          end
+
+          context 'when the current activity is active' do
+            before { tree.current_activity.stub(:active?).and_return(true) }
+            it 'terminates the current activity' do
+              process.should == [:exit, :previous]
+            end
+          end
+          
+          context 'when the current activity is not active' do
+            before { tree.current_activity.stub(:active?).and_return(false) }
+            it 'issues the previous sequencing request' do
+              process.should == [nil, :previous]
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  describe 'Choice navigation request' do
+    let(:req) { :choice }
+    let(:target) { 'target' }
+
+    context 'when the target activity does not exist' do
+      before { tree.activities = OpenStruct.new }
+      it_behaves_like 'sequencing exception'
+    end
+
+    context 'when the target activity is the root activity' do
+      before do
+        r = double('root activity')
+        tree.activities = {target => r}
+        r.stub(:root?).and_return(true)
+      end
+
+      context 'and the current activity is not defined' do
+        it 'issues the choice sequencing request' do
+          process.should == [nil, :choice]
+        end
+      end
+
+      context 'there is a current activity defined' do
+        context 'whose Sequencing Control Choice Exit is true' do
+          before do
+            c = double('current activity')
+            c.stub(:sequencing_control_choice_exit).and_return(true)
+            tree.current_activity = c
+          end
+
+          context 'when the current activity is active' do
+            before { tree.current_activity.stub(:active?).and_return(true) }
+            it 'terminates the current activity' do
+              process.should == [:exit, :choice]
+            end
+          end
+
+          context 'when the current activity is not active' do
+            before { tree.current_activity.stub(:active?).and_return(false) }
+            it 'issues the choice sequencing request' do
+              process.should == [nil, :choice]
+            end
+          end
+        end
+        
+        context 'whose Sequencing Control Choice Exit is false' do
+          before do
+            c = double('current activity')
+            c.stub(:sequencing_control_choice_exit).and_return(false)
+            tree.current_activity = c
+          end
+
+          context 'when the current activity is active' do
+            before { tree.current_activity.stub(:active?).and_return(true) }
+            it_behaves_like 'sequencing exception'
+          end
+
+          context 'when the current activity is not active' do
+            before { tree.current_activity.stub(:active?).and_return(false) }
+            it 'issues the choice sequencing request' do
+              process.should == [nil, :choice]
+            end
+          end
+        end
+      end
+    end
+
+    context 'when the target activity is not the root activity' do
+      context "whose parent's Sequencing Control Choice is false" do
+        before do
+          t = double('target activity')
+          t.stub(:root?).and_return(false)
+          t.stub(:parent).and_return(double('parent activity'))
+          tree.activities = {target => t}
+        end
+
+        context 'and Sequencing Control Choice for the parent activity is false' do
+          before { tree.activities[target].parent.stub(:sequencing_control_choice).and_return(false) }
+          it_behaves_like 'sequencing exception'
+        end
+
+        context 'and Sequencing Control Choice for the parent activity is true' do
+          before { tree.activities[target].parent.stub(:sequencing_control_choice).and_return(true) }
+
+          context "the current activity is defined" do
+            before { tree.current_activity = double('current activity') }
+
+            context 'whose Sequencing Control Choice Exit is false' do
+              before { tree.current_activity.stub(:sequencing_control_choice_exit).and_return(false) }
+
+              context 'when the current activity is active' do
+                before { tree.current_activity.stub(:active?).and_return(true) }
+                it_behaves_like 'sequencing exception'
+              end
+
+              context 'when the current activity is not active' do
+                before { tree.current_activity.stub(:active?).and_return(false) }
+                it 'issues the choice sequencing request' do
+                  process.should == [nil, :choice]
+                end
+              end
+            end
+
+            context 'whose Sequencing Control Choice Exit is true' do
+              before { tree.current_activity.stub(:sequencing_control_choice_exit).and_return(true) }
+
+              context 'when the current activity is active' do
+                before { tree.current_activity.stub(:active?).and_return(true) }
+                it 'terminates the current activity' do
+                  process.should == [:exit, :choice]
+                end
+              end
+
+              context 'whtn the current activity is not active' do
+                before { tree.current_activity.stub(:active?).and_return(false) }
+                it 'issues the choice sequencing request' do
+                  process.should == [nil, :choice]
+                end
+              end
+            end
+          end
+
+          context "the current activity is not defined" do
+            it 'issues the choice sequencing request' do
+              process.should == [nil, :choice]
+            end
+          end
+        end
+      end
+    end
+  end
+
+  describe 'Jump navigation request'
+  describe 'Exit navigation request'
+  describe 'Exit All navigation request'
+  describe 'Suspend All navigation request'
+end
