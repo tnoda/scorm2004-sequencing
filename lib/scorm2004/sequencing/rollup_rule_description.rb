0742c20768a3a81b681a2ad3135b44eeba0637ad
Takahiro Noda <takahiro.noda@gmail.com>
2013-02-21 15:28:00 +0900

    Add the Scorm2004::Sequencing/RollupRuleDescription module.
    This module will be included in the rollup rule class.

---
+++ lib/scorm2004/sequencing/rollup_rule_description.rb
@@ -0,0 +1,59 @@
+require 'scorm2004/sequencing'
+
+module Scorm2004
+  module Sequencing
+    module RollupRuleDescription
+      def action
+        action = (rule['rollup_action'] || {})['action']
+        case action
+        when /satisfied/i
+          'Satisfied'
+        when /notSatisfied/i
+          'Not Satisfied'
+        when /completed/i
+          'Completed'
+        when /incomplete/i
+          'Incomplete'
+        else
+          'Satisfied'
+        end
+      end
+
+      def condition_combination
+        combi = (rule['rollup_conditions'] || {})['condition_combination']
+        (/all/i =~ combi) ? 'All' : 'Any'
+      end
+
+      def child_activity_set
+        case rule['child_activity_set']
+        when /all/i
+          'All'
+        when /any/i
+          'Any'
+        when /none/i
+          'None'
+        when /atLeastCount/i
+          'At Least Count'
+        when /atLeastPercent/i
+          'At Least Percent'
+        else
+          'All'
+        end
+      end
+
+      def conditions
+        ((rule['rollup_conditions'] || {})['rollup_conditions'] || []).map do |condition|
+          RollupCondition.build(condition)
+        end
+      end
+
+      def minimum_percent
+        rule['minimum_percent'] || 0.0
+      end
+
+      def minimum_count
+        rule['minimum_count'] || 0
+      end
+    end
+  end
+end
---
+++ spec/scorm2004/sequencing/rollup_rule_description_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'scorm2004/sequencing/rollup_rule_description'
+
+describe Scorm2004::Sequencing::RollupRuleDescription do
+  subject do
+    r = double('rollup rule')
+    r.extend(Scorm2004::Sequencing::RollupRuleDescription)
+    r
+  end
+
+  shared_context 'rollup rule attributes' do |action, combi, set, min_percent, min_count|
+    let(:rule) do
+      { "child_activity_set" => set,
+        "minimum_count" => min_count,
+        "minimum_percent" => min_percent,
+        "rollup_conditions" => { "condition_combination" => combi },
+        "rollup_action" => { "action" => action }
+      }
+    end
+  end
+
+  shared_examples 'rollup rule' do |action, combi, set, min_percent, min_count, conditions|
+    before { subject.stub(:rule).and_return(rule) }
+    its(:action) { should == action }
+    its(:condition_combination) { should == combi}
+    its(:child_activity_set) { should == set }
+    its(:minimum_percent) { should == min_percent }
+    its(:minimum_count) { should == min_count }
+    its(:conditions) { should == conditions } if conditions
+  end
+
+  describe 'defaults' do
+    it_behaves_like 'rollup rule', 'Satisfied', 'Any', 'All', 0.0, 0, [] do
+      let(:rule) { {} }
+    end
+  end
+
+  describe 'example without conditions' do
+    it_behaves_like 'rollup rule', 'Completed', 'All', 'At Least Count', 0.5, 3, [] do
+      include_context 'rollup rule attributes', 'Completed', 'All', 'atLeastCount', 0.5, 3
+    end
+  end
+end
