9dfc62b3d5826b3592be727b6a6942a5b03069e9
Takahiro Noda <takahiro.noda@gmail.com>
2013-02-22 23:34:48 +0900

    Add the Scorm2004::Sequencing::ObjectiveRollupUsingDefaultRule class.

---
+++ lib/scorm2004/sequencing/objective_rollup_using_default_rule.rb
@@ -0,0 +1,40 @@
+require 'scorm2004/sequencing'
+require 'ostruct'
+
+module Scorm2004
+  module Sequencing
+    class ObjectiveRollupUsingDefaultRule
+      def call(activity)
+        return unless ro(activity)
+
+        # Apply the default not satisfied rule
+        nsb = bag(activity, 'Not Satisfied')
+        if !nsb.empty? && nsb.all? { |cro| cro && cro.progress_status }
+          ro(activity).progress_status = true
+          ro(activity).satisfied_status = false
+        end
+
+        # Apply the default satisfied rule
+        sb = bag(activity, 'Satisfied')
+        if !sb.empty? && sb.all? { |cro| cro && cro.progress_status && cro.satisfied_status }
+          ro(activity).progress_status = true
+          ro(activity).satisfied_status = true
+        end
+      end
+
+      private
+
+      def rolled_up_objective(activity)
+        activity.rolled_up_objective
+      end
+      alias :ro :rolled_up_objective
+
+      def bag_of_child_rolled_up_objective(activity, rollup_action)
+        activity.children.find_all { |c|
+          c.tracked? && c.included_in_rollup?(rollup_action)
+        }.map(&:rolled_up_objective)
+      end
+      alias :bag :bag_of_child_rolled_up_objective
+    end
+  end
+end
---
+++ spec/scorm2004/sequencing/objective_rollup_using_default_rule_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+require 'scorm2004/sequencing/objective_rollup_using_default_rule'
+
+describe Scorm2004::Sequencing::ObjectiveRollupUsingDefaultRule do
+  context 'without a rolled-up objective' do
+    let(:activity) do
+      a = double('activity')
+      a.stub(:rolled_up_objective).and_return(nil)
+      a
+    end
+
+    it 'does nothing' do
+      subject.call(activity)
+    end
+  end
+
+  shared_examples 'default rule' do |expected, children|
+    def fake_child(rolled_up_objective = nil, tracked_flag = true, included_flag = true)
+      c = double('child')
+      c.stub(:tracked? => tracked_flag, :included_in_rollup? => included_flag)
+      if rolled_up_objective
+        ro = double('rolled-up objective')
+        ro.stub(rolled_up_objective)
+        c.stub(:rolled_up_objective => ro)
+      end
+      c
+    end
+
+    context "activity has children: #{children}" do
+      let(:activity) do
+        a = double('activity')
+        a.stub(:children => children.map { |c| fake_child(*c)} )
+        a
+      end
+
+      it "might update Objective Progress/Satisfied with #{expected}" do
+        ro = double('activity rolled-up objective')
+        expected.each_slice(2) do |prog, sat|
+          ro.should_receive(:progress_status=).once.ordered.with(prog)
+          ro.should_receive(:satisfied_status=).once.ordered.with(sat)
+        end
+        activity.stub(:rolled_up_objective => ro)
+        subject.call(activity)
+      end
+    end
+  end
+
+  [ [[], [[nil, false, false], [nil, false, true]]],
+    [[], [[nil, true, false]]],
+    [[], [[{:progress_status => false}]]],
+    [[true, false],
+      [[{:progress_status => true, :satisfied_status => false },
+          {:progress_status => true, :satisfied_status => true }]]],
+    [[true, false, true, true],
+      [[{:progress_status => true, :satisfied_status => true},
+          {:progress_status => true, :satisfied_status => true}]]]
+  ].each do |*args|
+    it_behaves_like 'default rule', *args
+  end
+end
