5fc1b5389c1d91088959adf89f775d500b72daae
Takahiro Noda <takahiro.noda@gmail.com>
2013-03-01 23:43:43 +0900

    Add some require lines.

--- lib/scorm2004/sequencing.rb
+++ lib/scorm2004/sequencing.rb
@@ -1,4 +1,10 @@
 require "scorm2004/sequencing/version"
+require 'scorm2004/sequencing/sequencing_exception'
+require 'scorm2004/sequencing/process_factory'
+require 'scorm2004/sequencing/abstract_process'
+require 'scorm2004/sequencing/termination_request_process'
+require 'scorm2004/sequencing/sequencing_exit_action_rules_subprocess'
+require 'scorm2004/sequencing/terminate_descendent_attempts_process'
 
 module Scorm2004
   module Sequencing
