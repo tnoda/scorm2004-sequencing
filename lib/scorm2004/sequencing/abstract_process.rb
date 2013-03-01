ea1be39e5b1c6a654c63b2b70b663d3aebddaed8
Takahiro Noda <takahiro.noda@gmail.com>
2013-03-01 23:27:21 +0900

    Add the Scorm2004::Sequencng::AbstractProcess class.

---
+++ lib/scorm2004/sequencing/abstract_process.rb
@@ -0,0 +1,35 @@
+require 'scorm2004/sequencing'
+require 'scorm2004/sequencing/sequencing_exception'
+require 'scorm2004/sequencing/process_factory'
+require 'forwardable'
+
+module Scorm2004
+  module Sequencing
+    class AbstractProcess
+      include SequencingException
+
+      ACTIVITY_TREE_METHODS = [
+        :current_activity, :current_activity=,
+        :suspended_activity, :suspended_activity=,
+        :root_activity
+      ]
+
+      CURRENT_ACTIVITY_METHODS = [
+        :active?, :suspended?, :root?,
+        :end_attempt, :rollup, :check_sequencing_rules,
+        :parent
+      ]
+
+      extend Forwardable
+      def_delegators(:@tree, *ACTIVITY_TREE_METHODS)
+      def_delegators(:current_activity, *CURRENT_ACTIVITY_METHODS)
+      def_delegators(:@process_factory, *ProcessFactory::AVAILABLE_PROCESSES)
+
+      # @param tree [ActivityTree] the activity tree
+      def initialize(tree)
+        @tree = tree
+        @process_factory = ProcessFactory.new(tree)
+      end
+    end
+  end
+end
---
+++ spec/scorm2004/sequencing/abstract_process_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+require 'scorm2004/sequencing/abstract_process'
+
+describe Scorm2004::Sequencing::AbstractProcess do
+
+  describe 'has an activity tree' do
+    subject do
+      tree = double('tree')
+      Scorm2004::Sequencing::AbstractProcess::ACTIVITY_TREE_METHODS.each do |m|
+        tree.should_receive(m).and_return(:dummy)
+      end
+      described_class.new(tree)
+    end
+
+    it 'forwards some methods to the activity tree' do
+      Scorm2004::Sequencing::AbstractProcess::ACTIVITY_TREE_METHODS.each do |m|
+        subject.send(m).should == :dummy
+      end
+    end
+  end
+
+  describe 'has a current activity' do
+    subject do
+      current_activity = double('current activity')
+      Scorm2004::Sequencing::AbstractProcess::CURRENT_ACTIVITY_METHODS.each do |m|
+        current_activity.should_receive(m).and_return(:dummy)
+      end
+      tree = double('tree')
+      tree.stub(current_activity: current_activity)
+      described_class.new(tree)
+    end
+
+    it 'forwards some methods to the current activity' do
+      Scorm2004::Sequencing::AbstractProcess::CURRENT_ACTIVITY_METHODS.each do |m|
+        subject.send(m).should == :dummy
+      end
+    end
+  end
+
+  Scorm2004::Sequencing::ProcessFactory::ACTIVITY_TREE_PROCESSES.each do |m|
+    describe "##{m}" do
+      it 'instantiates the appropriate process class' do
+        klass = double('ProcessFactory')
+        klass.should_receive(:new).and_return("dummy process: #{m}")
+        stub_const('Scorm2004::Sequencing::' + m.to_s.split('_').map(&:capitalize).join(''), klass)
+        described_class.new(:dummy_activity_tree).send(m).should == "dummy process: #{m}"
+      end
+    end
+  end
+end
